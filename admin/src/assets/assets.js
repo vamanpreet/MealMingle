db52cd029ad6dd4c2977aecd4e2c1b6fb727cf25
vamanpreet <vamanpreet2110@gmail.com>
2024-08-20 19:17:58 +0530

    added admin portal

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,19 @@
+import logo from './logo.png'
+import logo_footer from './logo_footer.png'
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+import parcel_icon from './parcel_icon.png'
+
+export const assets ={
+    logo,
+    logo_footer,
+    add_icon,
+    order_icon,
+    profile_image,
+    upload_area,
+    parcel_icon
+}
+
+export const url = 'http://localhost:4000'
