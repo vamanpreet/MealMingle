f6835f941b78330add2b52c49a4a1be8adf13b3a
vamanpreet <vamanpreet2110@gmail.com>
2024-08-20 16:02:44 +0530

    backend initiated

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://vamanpreet2110:vamanpreet60@cluster0.652l5.mongodb.net/MealMingle?')
+    .then(() => console.log("Successfully connected to the database")); //mongodb.net/<dbname>?retryWrites=true&w=majority
+}
---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import cors from "cors"
+import {connectDB} from './config/db.js'
+import foodRouter from "./routes/foodRoute.js"
+
+
+// app config
+const app = express()
+const port = 4000
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'))
+
+
+
+app.listen(port, () => {
+    console.log(`MealMingle listening at http://localhost:${port}`)
+})
+
+// mongodb+srv://vamanpreet2110:vamanpreet60@cluster0.652l5.mongodb.net/?
+// retryWrites=true&w=majority&appName=Cluster0
