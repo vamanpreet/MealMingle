e09e4817504681a5c51c8bb82bf57d94d165359b
vamanpreet <vamanpreet2110@gmail.com>
2024-08-28 18:57:27 +0530

    bug

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const mongoURL = process.env.mongoURL;
 
 export const connectDB = async () => {
-    await mongoose.connect(mongoURL)
-    .then(() => console.log("Successfully connected to the database"));
-}
+    if (!mongoURL) {
+        console.error("MongoDB URI is not defined");
+        process.exit(1); // Exit the process with an error code
+    }
+
+    try {
+        await mongoose.connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Successfully connected to the database");
+    } catch (error) {
+        console.error("Error connecting to the database:", error.message);
+        process.exit(1); // Exit the process with an error code
+    }
+};
