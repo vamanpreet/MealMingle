55541a81a48393ddacaaee054eb4b2f199e2b046
vamanpreet <vamanpreet2110@gmail.com>
2024-08-28 18:50:09 +0530

    update

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
+const mongoURL = process.env.mongoURL;
+
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://vamanpreet2110:vamanpreet60@cluster0.652l5.mongodb.net/MealMingle?')
+    await mongoose.connect(mongoURL)
     .then(() => console.log("Successfully connected to the database"));
 }
